2aadfc22dd4b9365a665a70443ce8259fef1f36c
Pablo Ferrari <pabloferraricaliari@gmail.com>
2025-11-29 11:56:51 -0300

    refactor: remove eslint disable comment to top of file

--- src/shared/common/base-event.ts
+++ src/shared/common/base-event.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type Props<T> = { occurredOn: Date } & T;
 
 export abstract class BaseEvent<T> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   abstract readonly props: Props<T>;
   constructor(props: Props<T>) {}
 }
